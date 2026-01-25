c93c5ad9ecd111321ae1467c2c1f08972ba7d935
vishalabha1998@gmail.com <vishalabha1998@gmail.com>
2026-01-25 11:27:20 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/orderService/RestWelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.orderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWelcomeApplication.class, args);
+	}
+
+}
